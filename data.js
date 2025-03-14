4752f97c97f03a5ef1739e31f61dbc85f3b07b69
Nyashasya <figolanastasia2006@gmail.com>
2025-03-14 11:35:32 +0700

    Add project

---
+++ data.js
@@ -0,0 +1,59 @@
+const data = [
+  {
+    pic: "./assets/lara-woman.svg",
+    ruPhrase: "Ты говоришь по-японски?",
+    engPhrase: "Do you speak Japanese?",
+    phraseByWord: [
+      {
+        word: "Ты",
+        trans: ["you"],
+      },
+      {
+        word: "говоришь",
+        trans: ["(you) speak", "(you) are speaking", "(you) are talking"],
+      },
+      {
+        word: "по японски",
+        trans: ["Japanese"],
+        highlighted: true,
+      },
+    ],
+  },
+  {
+    pic: "./assets/olga-woman.svg",
+    ruPhrase: `Он из Автралии`,
+    engPhrase: "He comes from Australia",
+    phraseByWord: [
+      {
+        word: "Он",
+        trans: ["he", "him", "it"],
+      },
+      {
+        word: "из",
+        trans: ["come from", "is from", "has just"],
+        highlighted: true,
+      },
+      {
+        word: "Австралии",
+        trans: ["Australia"],
+      },
+    ],
+  },
+  {
+    pic: "./assets/victor-male.svg",
+    ruPhrase: `Я канадец`,
+    engPhrase: "I am Canadian",
+    phraseByWord: [
+      {
+        word: "я",
+        trans: ["I am"],
+      },
+
+      {
+        word: "Канадец",
+        trans: ["Canadian"],
+        highlighted: true,
+      },
+    ],
+  },
+];
